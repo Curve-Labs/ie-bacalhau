9415e6fbc37976fde377615c13052add87f799b7
fabianschu <48454910+fabianschu@users.noreply.github.com>
2022-12-22 09:28:09 +0100

    feat: sets up test avatar and reality module

--- packages/on-chain/hardhat.config.ts
+++ packages/on-chain/hardhat.config.ts
@@ -1,8 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "hardhat-deploy";
+
+import "./tasks/setup";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.7",
+        settings: {},
+      },
+    ],
+  },
+
+  external: {
+    contracts: [
+      {
+        artifacts: "../node_modules/@gnosis.pm/zodiac/dist/src/abi",
+      },
+    ],
+  },
 };
 
 export default config;
