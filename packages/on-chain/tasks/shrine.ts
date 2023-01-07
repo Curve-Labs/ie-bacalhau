b862313a3391f2096094f1dd8de5e55bad8ddcb7
fabianschu <48454910+fabianschu@users.noreply.github.com>
2023-01-07 10:50:32 +0100

    feat: generalizes shrine deploy task
    useful for testing

--- packages/on-chain/tasks/shrine.ts
+++ packages/on-chain/tasks/shrine.ts
@@ -4,10 +4,28 @@ task("shrine:setup", "Deploys a new instance of Shrine")
   .addParam(
     "avatar",
     "Address of the avatar (e.g. the Gnosis Safe)",
-    "undefined",
+    undefined,
     types.string
   )
-  .setAction(async ({ avatar }, { ethers }) => {
+  .addOptionalParam(
+    "ipfs",
+    "Ipfs hash that contains metadata of first merkle drop",
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    "root",
+    "Merkle root of first merkle drop",
+    undefined,
+    types.string
+  )
+  .addOptionalParam(
+    "shares",
+    "Total shares of first merkle drop",
+    undefined,
+    types.int
+  )
+  .setAction(async ({ avatar, ipfs, root, shares }, { ethers }) => {
     const Shrine = await ethers.getContractFactory("Shrine");
     const shrine = await Shrine.deploy();
 
@@ -17,15 +35,17 @@ task("shrine:setup", "Deploys a new instance of Shrine")
       `Shrine deployed with tx ${shrine.deployTransaction.hash} to address ${shrine.address}`
     );
 
-    // we initialize the shrine without a working merkle drop
-    // because the merkle drop is supposed to be initiated through reality
-    const initialLedger = {
-      merkleRoot: ethers.constants.HashZero,
-      totalShares: 1,
-    };
-    const initialIpfsHash = "0";
+    const initialLedger = { merkleRoot: root, totalShares: shares };
 
-    await shrine.initialize(avatar, initialLedger, initialIpfsHash);
+    // if root & shares are not set
+    // we initialize the shrine without a working merkle drop
+    // this is useful because the merkle drop is supposed to be initiated through reality
+    if (!(root && shares)) {
+      initialLedger.merkleRoot = ethers.constants.HashZero;
+      initialLedger.totalShares = 1;
+    }
+
+    await shrine.initialize(avatar, initialLedger, ipfs || "0");
 
     return shrine;
   });
