235276abeb0dde474d57c473c7e1a9fe40b5636d
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-01-07 18:11:56 +1100

    feat(off-chain): created a script to run and download results of bacalhau job

---
+++ packages/off-chain/shell/run_bacalhau.sh
@@ -0,0 +1,120 @@
+# shell script to run docker image on bacalhau
+
+# get latest Docker Image digest
+IMAGE_INFO=$(curl -X 'GET' https://hub.docker.com/v2/namespaces/mihirsinhparmar/repositories/bacalhau-ie/tags/latest)
+LATEST_DOCKER_IMAGE_DIGEST=`awk -F',"digest":"|","os"' '{print $2}' <<< "$IMAGE_INFO"`
+
+# Constants
+DOCKER_IMAGE=mihirsinhparmar/bacalhau-ie@$LATEST_DOCKER_IMAGE_DIGEST
+DEFAULT_HTTPS_URL=https://nftstorage.link/ipfs/bafybeiblg4ymbxuhpckcsfkdhn52o4l4iakc6wgjptp4fil4ctjcvdsvp4
+DEFAULT_IPFS_CID=bafybeiblg4ymbxuhpckcsfkdhn52o4l4iakc6wgjptp4fil4ctjcvdsvp4
+OUTPUT_FOLDER=results
+
+# create results directory
+mkdir ./$OUTPUT_FOLDER
+
+echo "Running docker image $DOCKER_IMAGE on bacalhau"
+
+echo ""
+
+# Ask if Input needs to be given
+echo "Provide inputs using (Not Case Sensitive) 
+A) IPFS 
+B) HTTPs 
+C) No Input 
+D) Default HTTPs Input 
+E) Default IPFS Input"
+read INPUT_URL_TYPE
+echo ""
+
+echo "Running Docker image on Bacalhau"
+if [ $INPUT_URL_TYPE == "A" -o $INPUT_URL_TYPE == "a" ]
+then
+   # Read Input URL (HTTPS)
+    echo "Provide IPFS Input:"
+    read INPUT_CID
+    JOB_OUTPUT=`bacalhau docker run --inputs $INPUT_CID $DOCKER_IMAGE`
+elif [ $INPUT_URL_TYPE == "B" -o $INPUT_URL_TYPE == "b" ]
+then
+   # Read Input URL (HTTPS)
+    echo "Provide HTTPs Input:"
+    read INPUT_HTTPS_URL
+    JOB_OUTPUT=`bacalhau docker run --input-urls $INPUT_HTTPS_URL $DOCKER_IMAGE`
+elif [ $INPUT_URL_TYPE == "C" -o $INPUT_URL_TYPE == "c" ]
+then
+   # Run docker image with no input
+   echo "No Input"
+   JOB_OUTPUT=$(bacalhau docker run $DOCKER_IMAGE)
+elif [ $INPUT_URL_TYPE == "D" -o $INPUT_URL_TYPE == "d" ]
+then
+   # Run docker image with default HTTPs input
+   echo "Default HTTPs Input"
+   JOB_OUTPUT=`bacalhau docker run --input-urls $DEFAULT_HTTPS_URL $DOCKER_IMAGE`
+elif [ $INPUT_URL_TYPE == "E" -o $INPUT_URL_TYPE == "e" ]
+then
+   # run docker image with default IPFS input
+   echo "Default IPFS Input"
+   JOB_OUTPUT=`bacalhau docker run --inputs $DEFAULT_IPFS_CID $DOCKER_IMAGE`
+else
+   # Invalid Input
+   echo "Invalid Input"
+fi
+
+# log output of Job
+echo $JOB_OUTPUT
+
+# Retrieve JOB ID from the JOB output
+JOB_ID=`awk -F'Job ID: | Checking job' '{print $2}' <<< "$JOB_OUTPUT"`
+
+# Clear results folder
+# This is a dangerous operation, need to consider all the edge cases like
+# 1. running from different directory
+# 2. error in relative directory
+# echo "Do you want to empty Results directory before downloading results"
+# echo "y | Y - to clear results directory"
+# echo "Hit enter to skip"
+# read CLEAR_DIRECTORY
+
+# if [ $CLEAR_DIRECTORY == "Y" -o $CLEAR_DIRECTORY == "y" ]
+# then
+#    # clear directory
+#    echo "Clearing result directory"
+#    rm -rv ./$OUTPUT_FOLDER/*
+# else
+#     # by Default skipping
+#     echo "Not clearing result directory"
+# fi
+
+
+# get results
+echo $JOB_ID
+echo "Fetching Results"
+
+# Generating short ID of JOB
+JOB_SHORT_ID=$(cut -d "-" -f 1 <<< $JOB_ID)
+RESULT_FOLDER="Result_$JOB_SHORT_ID"
+mkdir ./$OUTPUT_FOLDER/$RESULT_FOLDER
+echo $JOB_SHORT_ID
+
+OUTPUT_PATH=./$OUTPUT_FOLDER/$RESULT_FOLDER/Result-$JOB_SHORT_ID.json
+
+# fetch results and store
+bacalhau get $JOB_ID --output-dir ./$OUTPUT_FOLDER/$RESULT_FOLDER
+
+#  result via API to get the CID where result is stored
+curl -X 'POST' \
+  'http://bootstrap.production.bacalhau.org:1234/results' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "job_id": "'"${JOB_ID}"'"
+}' >> ./$OUTPUT_PATH
+
+# going back to root
+cd ../..
+
+# Format output
+yarn prettier -w ./packages/off-chain/$OUTPUT_FOLDER/$RESULT_FOLDER
+
+# going back to off-chain directory
+cd ./packages/off-chain
