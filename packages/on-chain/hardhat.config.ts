2aa1c2cff377a50b5ce42053dfa0afb60acf0675
fabianschu <48454910+fabianschu@users.noreply.github.com>
2023-01-11 11:14:35 +0100

    feat: imports custom seedphrase or pk

--- packages/on-chain/hardhat.config.ts
+++ packages/on-chain/hardhat.config.ts
@@ -1,9 +1,28 @@
+import dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
+import type { HttpNetworkUserConfig } from "hardhat/types";
 import "@nomicfoundation/hardhat-toolbox";
 
 import "./tasks/reality";
 import "./tasks/shrine";
 import "./tasks/test";
+
+dotenv.config();
+
+const { INFURA_KEY, SEEDPHRASE, ETHERSCAN_API_KEY, PK, ALCHEMY_KEY } =
+  process.env;
+
+const HARDHAT_DEFAULT_SEEDPHRASE =
+  "test test test test test test test test test test test junk";
+
+const sharedNetworkConfig: HttpNetworkUserConfig = {};
+if (PK) {
+  sharedNetworkConfig.accounts = [PK];
+} else {
+  sharedNetworkConfig.accounts = {
+    mnemonic: SEEDPHRASE || HARDHAT_DEFAULT_SEEDPHRASE,
+  };
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -16,6 +35,16 @@ const config: HardhatUserConfig = {
         settings: {},
       },
     ],
+  },
+  networks: {
+    mainnet: {
+      ...sharedNetworkConfig,
+      url: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+    },
+    goerli: {
+      ...sharedNetworkConfig,
+      url: `https://goerli.infura.io/v3/${INFURA_KEY}`,
+    },
   },
 };
 
