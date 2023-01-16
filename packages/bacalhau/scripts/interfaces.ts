b5f419ded743b416bbab6e9455407cfea2224560
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-01-16 17:06:20 +0530

    feat(bacalhau): added new interfaces for inputs based on cermaic stream

--- packages/bacalhau/scripts/interfaces.ts
+++ packages/bacalhau/scripts/interfaces.ts
@@ -1,12 +1,24 @@
 // @ts-ignore
-import { Address } from "ethers";
-
-export interface Contribution {
-  contributor: Address;
-  shares: number;
+import {Address} from "ethers";
+export interface IEResult {
+  newRewards: any;
+  newTrustSeed: any;
 }
 
-export interface Contributions {
-  totalShares: number;
-  contributions: Array<Contribution>;
+export interface InputData {
+  data: ContributionsData;
+  metadata?: string;
 }
+
+export interface ContributionsData {
+  [contributor: string]: number | string;
+}
+
+export interface IEInputs {
+  dataList: Array<InputData>;
+  trustedSeedList: Array<Array<Address>>;
+  previousRewards: string;
+  extraData?: Array<any> | undefined
+}
+
+export type MerkleTreeTuple = [Address, number | string];
