56d4c0c740cebfd4b868a6256822ac9bde3101cf
fabianschu <48454910+fabianschu@users.noreply.github.com>
2023-01-16 11:14:13 +0100

    feat: saves shrine deployment

--- packages/on-chain/tasks/shrine.ts
+++ packages/on-chain/tasks/shrine.ts
@@ -25,30 +25,42 @@ task("shrine:setup", "Deploys a new instance of Shrine")
     undefined,
     types.int
   )
-  .setAction(async ({ avatar, ipfs, root, shares }, { ethers }) => {
-    const Shrine = await ethers.getContractFactory("Shrine");
-    const shrine = await Shrine.deploy();
+  .setAction(
+    async ({ avatar, ipfs, root, shares }, { ethers, deployments }) => {
+      const { deploy } = deployments;
+      const [signer] = await ethers.getSigners();
 
-    await shrine.deployed();
+      const deployResult = await deploy("Shrine", {
+        from: signer.address,
+        args: [],
+        log: true,
+      });
 
-    console.log(
-      `Shrine deployed with tx ${shrine.deployTransaction.hash} to address ${shrine.address}`
-    );
+      console.log(
+        `Shrine deploying with tx ${deployResult.receipt?.transactionHash} to address ${deployResult.address}`
+      );
 
-    const initialLedger = { merkleRoot: root, totalShares: shares };
+      const shrine = await ethers.getContractAt(
+        "Shrine",
+        deployResult.address,
+        signer
+      );
 
-    // if root & shares are not set
-    // we initialize the shrine without a working merkle drop
-    // this is useful because the merkle drop is supposed to be initiated through reality
-    if (!(root && shares)) {
-      initialLedger.merkleRoot = ethers.constants.HashZero;
-      initialLedger.totalShares = 1;
+      const initialLedger = { merkleRoot: root, totalShares: shares };
+
+      // if root & shares are not set
+      // we initialize the shrine without a working merkle drop
+      // this is useful because the merkle drop is supposed to be initiated through reality
+      if (!(root && shares)) {
+        initialLedger.merkleRoot = ethers.constants.HashZero;
+        initialLedger.totalShares = 1;
+      }
+
+      await shrine.initialize(avatar, initialLedger, ipfs || "0");
+
+      return shrine;
     }
-
-    await shrine.initialize(avatar, initialLedger, ipfs || "0");
-
-    return shrine;
-  });
+  );
 
 task("shrine:offer", "Sends tokens to Shrine to be distributed via merkle drop")
   .addParam("token", "Token address", undefined, types.string)
