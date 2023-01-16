7c954b0d7be8ffe84a9763ba3b535610f9df153a
Mihirsinh Parmar <mihirsinh.parmar.it@gmail.com>
2023-01-16 17:09:38 +0530

    feat(bacalhau): functionality to iterate over rounds of data using the IE function used

--- packages/bacalhau/scripts/example.ts
+++ packages/bacalhau/scripts/example.ts
@@ -1,9 +1,18 @@
-import { Contribution } from "../scripts/interfaces";
+// @ts-ignore
+import {Address} from "ethers";
+import { IEResult, InputData } from "../scripts/interfaces";
 
-export function impactEvaluatorFunction(contributions: Array<Contribution>) {
+export function impactEvaluatorFunction(
+  dataList: Array<InputData>,
+  trustedSeedList: Array<Array<Address>>,
+  previousRewards: string,
+  extraData: Array<any> | undefined = undefined
+): IEResult {
   console.log("Example Impact Evaluator Function");
-  return contributions.map((contribution) => ({
-    contributor: contribution.contributor,
-    shares: contribution.shares * 100,
-  }));
+  let result: IEResult = {
+    newRewards: dataList.reduce((output,data)=>({...output, ...data.data}), {}),
+    newTrustSeed: trustedSeedList.reduce((output, t)=>[...output, ...t], []),
+  };
+  console.log(result);
+  return result;
 }
--- packages/bacalhau/scripts/index.ts
+++ packages/bacalhau/scripts/index.ts
@@ -1,119 +1,217 @@
+import {
+  checkAndCreateIfDoesntExist,
+  logProgress,
+  logResult,
+  logRoundStart,
+} from "./../utils/IO";
 // to be run on bacalhau via Docker container
 // file system is used to fetch the data from ../inputs/ directory
-import { Contribution, Contributions } from "./interfaces";
-import { readJson, writeJson, readDir, log, logError } from "./../utils/IO";
+import { IEInputs, IEResult, MerkleTreeTuple } from "./interfaces";
+import {
+  readJson,
+  writeJson,
+  readDir,
+  log,
+  logError,
+  isDirectory,
+  filesExist,
+  logSuccess,
+} from "./../utils/IO";
 import { getMerkleTree } from "../utils/merkleTree";
 import {
   inputPath,
-  metadataOutputPath,
   merkleTreeOutputPath,
   rewardsOutputPath,
-  dataFilePath,
+  trustSeedOutputPath,
+  outputPath,
 } from "./constants";
+import { StandardMerkleTree } from "@openzeppelin/merkle-tree";
 
 // impact evaluator function cannot be asynchronous function
 // todo: find how we main() can handle asynchronous functions as well
 export function main(impactEvaluatorFunction: any) {
-  // read inputs
-  log("Reading inputs now...");
+  // reading contents of "../inputs" directory
+  const contents = readDir(inputPath);
+  log("Input directory contains following files:");
+  log(contents);
+  // if length is 0, no fils exist in input directory
+  if (contents.length === 0) {
+    log("No input was found");
+  }
 
-  let dataInput: Array<Contribution>;
-  try {
-    // reading contents of "../inputs" directory
-    const contents = readDir(inputPath);
-    log("Input directory contains following files:");
-    log(contents);
-    // if length is 0, no fils exist in input directory
-    if (contents.length === 0) {
-      log("No input was found");
+  // run loop for each round (each folder in ./inputs represent a round)
+  for (const round of contents) {
+    logRoundStart(round);
+
+    // check if the content being scanned is directory or not.
+    // inputs should be inside a directory only
+    if (!isDirectory(`${inputPath}/${round}`)) {
+      logError(`Skipping ${round} as it is not a folder.`);
+      continue;
+    }
+    // check if directory contains necessary files
+    if (!filesExist(inputPath, round)) {
+      logError(`Skipping ${round}`);
+      continue;
     }
 
-    // if function input file found, parsing the JSON data of function input file ("./ImpactEvaluatorFunction.json").
-    // input data should be in file "./Contributions.json"
-    let data: Contributions;
+    // execute
+    // read data
+    // read inputs
+    logProgress("Reading inputs now");
+    const round_contents = readDir(`${inputPath}/${round}/`);
 
-    // read input data
-    dataInput = readJson(dataFilePath);
-
-    // fetch data
-    if (dataInput.length !== undefined || dataInput.length !== 0) {
-      const totalShares = dataInput.reduce(
-        (totalShares, contribution) => contribution.shares + totalShares,
-        0
-      );
-      data = {
-        totalShares: totalShares,
-        contributions: dataInput,
-      };
-    } else {
-      // if now data found, what evaluation can be made
-      throw Error("No data found to be evaluated");
-    }
-
-    // run ie function
-    const inputParams = [data.contributions];
-    const result = impactEvaluatorFunction(...inputParams);
-
-    // log result
-    log("Rewards after running IE Function:");
-    log(result);
-
-    // generate merkle tree
-    const inputForMerkleTree = result.map((contribution: Contribution) => [
-      contribution.contributor,
-      contribution.shares,
-    ]);
-    const merkleTree = getMerkleTree(inputForMerkleTree);
-
-    // write outputs
-    merkleTree.root;
-    const metadataOutput = {
-      success: true,
-      merkleRoot: merkleTree.root,
-      contributions: data,
+    const round_inputs: IEInputs = {
+      dataList: [],
+      trustedSeedList: [],
+      previousRewards: "",
+      extraData: undefined,
     };
-    const merkleTreeOutput = merkleTree.dump();
-    const rewardsOutput = {
-      rewards: result,
-    };
-    log("Writing outputs");
+
     try {
-      // write metadata
-      log("Writing metadata outputs to", metadataOutputPath);
-      writeJson(metadataOutputPath, metadataOutput);
-
-      // write merkle tree
-      log("Writing merkle tree outputs to", merkleTreeOutputPath);
-      writeJson(merkleTreeOutputPath, merkleTreeOutput);
-
-      // write rewards
-      log("writing rewards to:", rewardsOutputPath);
-      writeJson(rewardsOutputPath, rewardsOutput);
+      // read data
+      for (const content of round_contents) {
+        const parsedData = readJson(`${inputPath}/${round}/${content}`);
+        if (content.includes("data")) {
+          logSuccess("data found in ", content, round);
+          round_inputs.dataList.push(parsedData);
+        } else if (content.includes("trustedSeed")) {
+          logSuccess("trsusted seed found in ", content, round);
+          round_inputs.trustedSeedList.push(parsedData);
+        } else if (content.includes("previousRewards")) {
+          logSuccess("previousRewards found in ", content, round);
+          round_inputs.previousRewards = parsedData;
+        } else {
+          logError("Unexpected file found: ", content);
+        }
+      }
     } catch (e) {
-      // some issue faced while writing
-      log("Writing output failed");
+      logError(`Reading Inputs for ${round} failed`);
       logError(e);
     }
 
-    // for test purpose only, to be removed after enough confidence is achieved
-    log("reading output file");
+    logResult("Inputs for this round are:", round_inputs);
+
+    // // pass data
+    logProgress(`Running IE function on data in: ${round}`);
+    let resultForCurrentRound: IEResult | undefined;
     try {
-      const metadataResult = readJson(metadataOutputPath);
-      const merkleTreeResult = readJson(merkleTreeOutputPath);
-      const rewardsResult = readJson(rewardsOutputPath);
+      // todo: collect and pass extra data
+      resultForCurrentRound = impactEvaluatorFunction(
+        round_inputs.dataList,
+        round_inputs.trustedSeedList,
+        round_inputs.previousRewards
+      );
+    } catch (e) {
+      // to ensure an error can be thrown to stop execution
+      resultForCurrentRound = undefined;
+      logError("Error while executing IE function");
+      logError(e);
+    }
+
+    // cannot progress ahead if IE function execution failed
+    if (resultForCurrentRound === undefined) {
+      throw Error("Error in IE function execution");
+    }
+
+    logSuccess("Impact Evaluation function successfully executed.");
+    logResult("IE output:", resultForCurrentRound);
+
+    // result should contain data as following
+    // result will now contain two properties
+    // 1. newRewards
+    // 2. newTrsutedSeed
+
+    // generate merkle tree
+    logProgress("Preparing inputs for merkle tree");
+
+    let inputForMerkleTree: Array<MerkleTreeTuple> = [];
+
+    // new rewards needs to be an object
+    if (
+      typeof resultForCurrentRound.newRewards !== "object" ||
+      resultForCurrentRound.newRewards === null
+    ) {
+      throw Error("New Rewards in IE output is not an object");
+    }
+
+    // as new rewards is an object, we first prepare an array of all the keys
+    const contributors = Object.keys(resultForCurrentRound.newRewards);
+
+    // then we loop over the keys to convert them into a format which can be passed into merkle tree generator
+    try {
+      for (const contributor of contributors) {
+        inputForMerkleTree.push([
+          contributor,
+          resultForCurrentRound.newRewards[contributor],
+        ]);
+      }
+    } catch (e) {
+      logError("Error in formatting IE output to merkle tree generator output");
+      logError(e);
+    }
+    logResult("Input ready for merkle tree:", inputForMerkleTree);
+
+    logProgress("Generating Merkle Tree");
+    let merkleTree: StandardMerkleTree<string[]> | undefined;
+    try {
+      merkleTree = getMerkleTree(inputForMerkleTree);
+    } catch (e) {
+      // to ensure an error can be thrown to stop execution
+      merkleTree = undefined;
+      logError("Error in generating merkle tree");
+      logError(e);
+    }
+    logSuccess("Merkle tree generated!");
+
+    // cannot progress ahead without merkle tree generation
+    if (merkleTree === undefined) {
+      throw Error("Error in Merkle tree generation");
+    }
+
+    // write data
+
+    // check if output directory exists
+    checkAndCreateIfDoesntExist(outputPath(round), "Output");
+
+    // 1. merkle tree
+    const treeOutput = merkleTree.dump();
+    try {
+      writeJson(merkleTreeOutputPath(round), treeOutput);
+    } catch (e) {
+      logError("Failed at writing output for Merkle Tree at round:", round);
+      logError(e);
+    }
+    // 2. new rewards
+    try {
+      writeJson(rewardsOutputPath(round), resultForCurrentRound.newRewards);
+    } catch (e) {
+      logError("Failed at writing output for New Rewards at round:", round);
+      logError(e);
+    }
+    // 3. new trusted seed
+    try {
+      writeJson(trustSeedOutputPath(round), resultForCurrentRound.newTrustSeed);
+    } catch (e) {
+      logError("Failed at writing output for new Trust Seed at round:", round);
+      logError(e);
+    }
+
+    logProgress("reading output file");
+    try {
+      const newTrustSeed = readJson(trustSeedOutputPath(round));
+      const merkleTreeResult = readJson(merkleTreeOutputPath(round));
+      const rewardsResult = readJson(rewardsOutputPath(round));
       log({
-        metadata: metadataResult,
+        newTrustSeed: newTrustSeed,
         merkleTree: merkleTreeResult,
         rewards: rewardsResult,
       });
     } catch (e) {
-      log("Reading output file failed");
+      logError("Reading output file failed");
       logError(e);
     }
 
-    log("Voila!");
-  } catch (e) {
-    log("Reading failed");
-    logError(e);
+    logSuccess("Voila!");
   }
 }
