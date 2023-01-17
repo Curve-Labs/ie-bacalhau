0e2383c287c795ef33c6710973811d261527588a
Zitzak <marvin88gr@gmail.com>
2023-01-17 20:31:50 +0100

    feat: create Dao Contribution Model
    The createDaoContributionModel.mts file can be used to create
    schemas, definition of a model. It also deploys the model and
    exports it to the /packages/datamodel/models/ directory.

---
+++ packages/datamodel/lib/config/config.ts
@@ -0,0 +1,3 @@
+// this Ceramic node endpoint is only for the testnetwork. For production we have to find a
+// commercial endpoint or host our own node to ensure long-term persistent data
+export const API_URL: string = "https://ceramic-clay.3boxlabs.com";
